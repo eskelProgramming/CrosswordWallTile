d95edaf799e0341826913b9f493f758aa2a62e2d
eskelProgramming <eskelley0673@students.cptc.edu>
2024-10-18 13:19:32 -0700

    Created and documented the Stain class

---
+++ CrosswordWallTile/Models/Stain.cs
@@ -0,0 +1,28 @@
+﻿namespace CrosswordWallTile.Models
+{
+    /// <summary>
+    /// Represents a single stain that can be applied to a wall tile.
+    /// </summary>
+    public class Stain
+    {
+        /// <summary>
+        /// The unique identifier for the stain. Tracked by EF Core.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the Stain.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The price of the Stain.
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The location of the image for the Stain.
+        /// </summary>
+        public string StainImage { get; set; }
+    }
+}
