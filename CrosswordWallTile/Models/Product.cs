1478ea55065b2d95a91ca46f482a9b6c36655cdd
eskelProgramming <eskelley0673@students.cptc.edu>
2024-10-18 13:06:11 -0700

    Created and documented the Product class

---
+++ CrosswordWallTile/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿namespace CrosswordWallTile.Models
+{
+    /// <summary>
+    /// An interface for a Product
+    /// </summary>
+    public interface Product
+    {
+        /// <summary>
+        /// The name of the product
+        /// </summary>
+        public String Name { get; set; }
+
+        /// <summary>
+        /// The consumer price of the product
+        /// </summary>
+        public Double Price { get; set; }
+
+        /// <summary>
+        /// The string that points to the image of the product
+        /// </summary>
+        public String ProductImage { get; set; }
+
+        /// <summary>
+        /// The number of this product in the cart
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// The subtotal of the quantity times the price
+        /// </summary>
+        public Double Subtotal
+        {
+            get
+            {
+                return Quantity * Price;
+            }
+        }
+
+        /// <summary>
+        /// The description of the product
+        /// </summary>
+        public String Description { get; set; }
+    }
+}
