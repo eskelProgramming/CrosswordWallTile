8a600ce61ffafe8780a53868e5e2687cc89825f5
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-10-23 14:47:59 -0800

    Created and documented the CrosswordHelper class.

---
+++ CrosswordWallTile/Models/CrosswordHelper.cs
@@ -0,0 +1,21 @@
+﻿using CrosswordWallTile.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrosswordWallTile.Models
+{
+    /// <summary>
+    /// A static helper class for the Crossword project. 
+    /// </summary>
+    public static class CrosswordHelper
+    {
+        /// <summary>
+        /// A list of all stains in the database.
+        /// </summary>
+        public static List<Stain> Stains { get; set; }
+
+        /// <summary>
+        /// A DbContext object that is used to interact with the database.
+        /// </summary>
+        public static DbContext _context = new ApplicationDbContext();
+    }
+}
