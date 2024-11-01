18961edff6aa15fd136faf6c6bcfcc36d2327341
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-11-01 14:17:51 -0700

    Added Product controller

---
+++ CrosswordWallTile/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using CrosswordWallTile.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrosswordWallTile.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<IActionResult> ProductsAsync()
+        {
+            List<IProduct> products = await CrosswordHelper.GetAllProductsAsync();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult CreateFrame()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFrame(Frame frame)
+        {
+            // Check if the frame is valid
+            if (!ModelState.IsValid)
+            {
+                return View(frame);
+            }
+
+            // Add the frame to the database
+            await CrosswordHelper.AddFrameAsync(frame);
+            return RedirectToAction("Products");
+        }
+    }
+}
