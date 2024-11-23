3f396582ed0d7d0f397a421ab3fe311e2affa0c1
Emily Skelley <ESkelley0673@students.cptc.edu>
2024-11-23 14:50:33 -0800

    Published to Azure

---
+++ CrosswordWallTile/Data/Migrations/20241123211036_identity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrosswordWallTile.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GridUnitOfMeasurment",
+                table: "Grids",
+                newName: "GridUnitOfMeasurement");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GridUnitOfMeasurement",
+                table: "Grids",
+                newName: "GridUnitOfMeasurment");
+        }
+    }
+}
